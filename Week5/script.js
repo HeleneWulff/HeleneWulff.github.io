3c95af481f4b428bd296d8a5735781958154e412
helenesvendsen <202109041@post.au.dk>
2024-04-18 12:22:04 +1000

    js week5

--- Week5/script.js
+++ Week5/script.js
@@ -1,2 +1,15 @@
-let myName = "helene";
-console.log("hello, my name is", myName);
+let tempInput = document.querySelector("#temperature");
+let temperature = temperature.value;
+console.log(temperature);
+function checkWeather() {
+  // let temperature = prompt("What is the temperature outside?");
+  if (temperature >= 20 && temperature < 30) {
+    console.log("it feels sunny and warm");
+  } else if (temperature >= 10 && temperature < 20) {
+    console.log("it feels cold");
+  } else if (temperature >= 30) {
+    console.log("it is boiling hot");
+  } else if (temperature < 10) {
+    console.log("it is freezing");
+  }
+}
