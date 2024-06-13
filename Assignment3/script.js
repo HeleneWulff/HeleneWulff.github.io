289d8056db749eaddd4d0cc87d7473426e1b5aac
helenesvendsen <s4077141@student.rmit.edu.au>
2024-06-13 14:26:50 +1000

    ...

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -5,5 +5,5 @@ checkoutButton.addEventListener("click", gotodetails);
 
 function gotodetails() {
   window.location.href =
-    "http://127.0.0.1:3000/Assignment3/Navigation/index.html";
+    "https://helenewulff.github.io/Assignment3/Navigation/";
 }
