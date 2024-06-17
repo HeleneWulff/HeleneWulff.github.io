a6dc181cedfcc4595f34b1854772e06b59c87c77
helenesvendsen <s4077141@student.rmit.edu.au>
2024-06-17 13:22:25 +1000

    changes

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -7,3 +7,5 @@ function gotodetails() {
   window.location.href =
     "https://helenewulff.github.io/Assignment3/Navigation/";
 }
+// This function is how the click and navigation work together.
+// I have added the event listener that when the user clicks on the checkout button they are being directed to the details page.
